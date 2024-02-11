d80f0784938290ebf457d0fb2c60d28c0ecf17d7
Rakib mia <rbepari404@gmail.com>
2024-02-11 01:26:54 -0600

    show all todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
-
 import shortid from "shortid";
-
 import CreateTodo from "./components/createTodo/CreateTodo";
-// import ShowTodos from "./components/showTodos/ShowTodos";
+import ShowTodos from "./components/showTodos/ShowTodos";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import coverImage from "./assets/images/cover.jpg";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
@@ -49,43 +48,56 @@ const App = () => {
   };
 
   return (
-    <div id="mainRoot">
-      <CreateTodo
-        value={value}
-        setValue={setValue}
-        setCount={setCount}
-        count={count}
-        getTodo={getTodo}
-      />
-      <div className="flex justify-center">
-        <select
-          onChange={handleChnage}
-          className="bg-[#14b8a6] text-white outline-none font-semibold mb-4 px-5 py-2"
-        >
-          <option value="all">All</option>
-          <option value="complate">Complate</option>
-          <option value="inConplate">In Complate</option>
-        </select>
+    <div
+      className="max-w-[1300px] relative mx-auto min-h-screen -mt-3"
+      style={{
+        backgroundImage: `url(${coverImage})`,
+        backgroundPosition: "cover",
+        backgroundSize: "cover",
+        width: "100%",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="absolute bg-[#00000099] top-0 w-full h-full ">
+        <div className="mt-12">
+          <CreateTodo
+            value={value}
+            setValue={setValue}
+            setCount={setCount}
+            count={count}
+            getTodo={getTodo}
+          />
+        </div>
+        <div className="flex justify-center">
+          <select
+            onChange={handleChnage}
+            className="bg-[#14b8a6] text-white outline-none font-semibold mb-4 px-5 py-2"
+          >
+            <option value="all">All</option>
+            <option value="complate">Complate</option>
+            <option value="inConplate">In Complate</option>
+          </select>
+        </div>
+        <ShowTodos
+          handleUpdate={handleUpdate}
+          filterText={filterText}
+          TodoComplate={TodoComplate}
+          hanndleDelete={hanndleDelete}
+          todos={todos}
+        />
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </div>
-      {/* <ShowTodos
-        handleUpdate={handleUpdate}
-        filterText={filterText}
-        TodoComplate={TodoComplate}
-        hanndleDelete={hanndleDelete}
-        todos={todos}
-      /> */}
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
     </div>
   );
 };
